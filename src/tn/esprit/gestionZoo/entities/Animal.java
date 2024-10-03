54691674c7c4b5ed555eeae189baa5c09e935438
Med Amin <brahmimedamin7@gmail.com>
2024-10-03 19:44:48 +0100

    prosit 4 commit

--- src/tn/esprit/gestionZoo/entities/Animal.java
+++ src/tn/esprit/gestionZoo/entities/Animal.java
@@ -48,7 +48,7 @@ public class Animal {
     public Animal() {
     }
 
-    Animal (String family, String name, int age, Boolean isMammal){
+    public Animal (String family, String name, int age, Boolean isMammal){
         this.family=family;
         this.name=name;
         this.age=age;
